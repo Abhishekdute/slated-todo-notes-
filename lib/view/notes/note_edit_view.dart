007c70f4e014f11ff2ed7176b65bd19273ab16b2
Abhishekdute <abhishekkumar33694@gmail.com>
2026-04-21 15:33:40 -0700

    Complete Premium Upgrade: Added Slated Branding, Notes feature, Multimedia support, Custom Logo, Adaptive Icons, and Professional Splash Screen

--- lib/view/notes/note_edit_view.dart
+++ lib/view/notes/note_edit_view.dart
@@ -1,10 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:provider/provider.dart';
-import 'package:animate_do/animate_do.dart';
 
-import '../../core/user/user_manager.dart';
 import '../../main.dart';
 import '../../models/note_model.dart';
 import '../../utils/colors.dart';
